b72afdb88f04f13e194c941a5e8a6a3387d606ee
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-05-21 16:46:40 +0900

    setting: nativewind, font 세팅

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { useLayoutEffect } from "react";
+import "../global.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    PyeojinGothic: require("../assets/fonts/default/PyeojinGothic-Medium.ttf"),
+  });
+
+  useLayoutEffect(() => {
+    if (fontsLoaded) {
+      // SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; // 로딩 중
+  }
+
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>reset</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-lg font-pyeojin text-black">
+        커스텀 폰트 적용됨!
+      </Text>
     </View>
   );
 }
