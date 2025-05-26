195f120192bd8633551699f1ba52dcd9fc6f865c
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-05-26 16:27:59 +0900

    feat: 스탬프 컴포넌트 분리 및 분기처리 완료

--- components/BoardGrid.tsx
+++ components/BoardGrid.tsx
@@ -1,31 +1,10 @@
+import useImageLoadTrack from "@/hooks/useImageLoadTrack";
 import { Board } from "@/types/zustand";
-import { memo, useCallback, useEffect, useState } from "react";
-import { FlatList, Pressable, Text, View } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
 import tw from "twrnc";
-
-// 개별 스탬프 컴포넌트, React.memo로 불필요한 재렌더링 방지
-const Stamp = memo(function Stamp({
-  filled,
-  fillBG,
-  emptyBG,
-  onPress,
-}: {
-  filled: boolean;
-  fillBG: string;
-  emptyBG: string;
-  onPress: () => void;
-}) {
-  return (
-    <Pressable onPress={onPress} style={tw`m-0.5`}>
-      <View
-        style={[
-          tw`border rounded w-10 h-10`,
-          { backgroundColor: filled ? fillBG : emptyBG },
-        ]}
-      />
-    </Pressable>
-  );
-});
+import Loading from "./Loading";
+import Stamp from "./Stamp";
 
 export default function BoardGrid({ board }: { board: Board }) {
   const [localStamps, setLocalStamps] = useState(board.stamps);
@@ -49,32 +28,32 @@ export default function BoardGrid({ board }: { board: Board }) {
     [setLocalStamps]
   );
 
-  const numColumns = Math.floor(Math.sqrt(board.stamps.length)) || 1;
-
   return (
     <View style={tw`mt-5`}>
       <Text style={tw`w-full text-center text-lg font-bold mb-2`}>
         {board.title}
       </Text>
 
-      <FlatList
-        data={localStamps}
-        numColumns={numColumns}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <Stamp
-            filled={item}
-            fillBG={board.fillBG}
-            emptyBG={board.emptyBG}
-            onPress={() => handleToggle(index)}
-          />
-        )}
-        scrollEnabled={false} // 스크롤 불필요하면 끄기
-        removeClippedSubviews={true} // 성능 향상
-        initialNumToRender={20}
-        maxToRenderPerBatch={20}
-        windowSize={21}
-      />
-    </View>
+        <FlatList
+          data={localStamps}
+          numColumns={board.stampCount}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <Stamp
+              filled={item}
+              fillBG={board.fillBG}
+              emptyBG={board.emptyBG}
+              onPress={() => handleToggle(index)}
+              handleImageComplete={handleImageComplete}
+            />
+          )}
+          scrollEnabled={false} // 스크롤 불필요하면 끄기
+          removeClippedSubviews={true} // 성능 향상
+          initialNumToRender={20}
+          maxToRenderPerBatch={20}
+          windowSize={21}
+        />
+      </View>
+    </>
   );
 }
---
+++ components/Stamp.tsx
@@ -0,0 +1,49 @@
+import { isImageFormat, isValidColor } from "@/utils/formatUtils";
+import { Image } from "expo-image";
+import { memo, useState } from "react";
+import { Pressable, View } from "react-native";
+import tw from "twrnc";
+
+export default memo(function Stamp({
+  filled,
+  fillBG,
+  emptyBG,
+  onPress,
+}: {
+  filled: boolean;
+  fillBG: string;
+  emptyBG: string;
+  onPress: () => void;
+}) {
+  const uri = filled ? fillBG : emptyBG;
+  const [hasError, setHasError] = useState(false);
+  const sizeStyle = tw`w-10 h-10 rounded`;
+
+  return (
+    <Pressable onPress={onPress} style={tw`m-0.5`}>
+      {isValidColor(uri) ? (
+        <View style={[sizeStyle, { backgroundColor: uri }]} />
+      ) : isImageFormat(uri) ? (
+        hasError ? (
+          <View
+            style={[
+              sizeStyle,
+              { backgroundColor: filled ? "rgba(0,0,0,0.5)" : "white" },
+            ]}
+          />
+        ) : (
+          <Image
+            source={{ uri }}
+            style={sizeStyle}
+            contentFit="cover"
+            onError={() => setHasError(true)}
+            cachePolicy="disk"
+            transition={100}
+          />
+        )
+      ) : (
+        <View style={[sizeStyle, { backgroundColor: "gray" }]} />
+      )}
+    </Pressable>
+  );
+});
